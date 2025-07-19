42d6cf750cf20e3703f4f61608dddd32defe0839
student sanket25 <jadhavsanket5656@gamil.com>
2025-07-19 12:40:25 -0400

    added the song models

---
+++ BACKEND/src/Models/Song.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const songSchema = new mongoose.Schema({
+    title:String,
+    artist: String,
+    album: String,
+})
+
+const Song = mongoose.model('Song', songSchema);
+
+module.exports = Song;
---
+++ BACKEND/src/Router/Song.Routers.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+
+
+router.post('/songs', async (req, res) => {
+
+})
+
+
+
+
+
+module.exports = router;
